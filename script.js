05c9628c4ef8a465687fa1922806d64c7e1db496
ganeshvrma <135155713+ganeshvrma@users.noreply.github.com>
2025-03-08 15:27:07 +0530

    feat/all done from side

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleBtn = document.querySelector(".navbar-toggler"); // Correct selector
-  const navbar = document.querySelector(".navbar-collapse");
+// document.addEventListener("DOMContentLoaded", function () 
+// {
+//   const toggleBtn = document.querySelector(".navbar-toggler"); // Correct selector
+//   const navbar = document.querySelector(".navbar-collapse");
 
-  if (toggleBtn && navbar) {
-      toggleBtn.addEventListener("click", function () {
-          navbar.classList.toggle("active"); // Use a proper class
-      });
-  }
-});
+//   if (toggleBtn && navbar) {
+//       toggleBtn.addEventListener("click", function () {
+//           navbar.classList.toggle("active "); // Use a proper class
+//       });
+  
+// document.addEventListener("click", function (event) {
+//   if (!navbar.contains(event.target) && event.target!==navbar) {
+//     navbar.classList.add("show"); 
+//   }
+// });
+// }});
